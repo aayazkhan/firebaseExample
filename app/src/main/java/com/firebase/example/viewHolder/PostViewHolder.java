fcf1318a055a7ebbcafb4316cddb7a2f0247dbae
Ayyaz Khan <khan.shan803@gmail.com>
2018-11-11 23:01:39 +0530

    third commit

--- app/src/main/java/com/firebase/example/viewHolder/PostViewHolder.java
+++ app/src/main/java/com/firebase/example/viewHolder/PostViewHolder.java
@@ -29,7 +29,7 @@ public class PostViewHolder extends RecyclerView.ViewHolder {
     public void setImage(final Context context, final String imageURL) {
         final ImageView imageView = (ImageView) itemView.findViewById(R.id.imgViewtPostImage);
 
-        Picasso.with(context).load(imageURL).networkPolicy(NetworkPolicy.OFFLINE).placeholder(R.drawable.image_background).into(imageView, new Callback() {
+        Picasso.with(context).load(imageURL).networkPolicy(NetworkPolicy.OFFLINE).into(imageView, new Callback() {
             @Override
             public void onSuccess() {
 
